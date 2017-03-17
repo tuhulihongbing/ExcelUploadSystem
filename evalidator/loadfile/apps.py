5f605bc81639b0ca02bc1ebdb026050cc8c02afc
tuhulihongbing <lihongbing@tuhu.cn>
2017-03-17 13:15:00 +0800

    no

--- evalidator/loadfile/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LoadfileConfig(AppConfig):
-    name = 'loadfile'
--- evalidator/static/reporting/custom.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by trains on 2017/3/3.
- */
-function PostBack( a ) {
-    mainpage = document.getElementById("mainpage");
-    mainpage.innerHTML = mainpage.innerHTML+"<h4>Please select a file !</h4>"
-}
